f9037e053c96f0cd44d174537c987d040418417b
yaruliu <yarusophia@gmail.com>
2019-10-14 23:30:01 -0400

    change data between revenue and profit per second

--- js/main.js
+++ js/main.js
@@ -7,12 +7,30 @@ var margin = { left:40, right:20, top:10, bottom:30 };
 var width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
+var flag = true
+
 var g = d3.select("#chart-area")
     .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
     .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("transform", " translate(" + margin.left + "," + margin.top + ")")
+
+var xAxisGroup = g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+
+var yAxisGroup = g.append("g")
+    .attr("class", "y axis")
+
+// X Scale
+var x = d3.scaleBand()
+    .range([0, width])
+    .padding(0.2)
+
+// Y Scale
+var y = d3.scaleLinear()
+    .range([height, 0])
 
 // X Label
 g.append("text")
@@ -23,7 +41,7 @@ g.append("text")
     .text("Month")
 
 // Y Label
-g.append("text")
+var yLabel = g.append("text")
     .attr("y", -60)
     .attr("x", -(height / 2))
     .attr("font-size", "20px")
@@ -35,42 +53,56 @@ d3.json("data/revenues.json").then(data => {
 
     data.forEach(d => {
         d.revenue = +d.revenue
+        d.profit = +d.profit
     });
 
-    var x = d3.scaleBand()
-        .domain(data.map(d => {return d.month}))
-        .range([0, width])
-        .padding(0.2)
+    d3.interval(() => {
+        update(data)
+        flag = !flag
+    }, 1000)
 
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => { return d.revenue })])
-        .range([height, 0])
+    update(data)
+})
+
+function update(data) {
+    var value = flag ? "revenue" : "profit"
+
+
+    x.domain(data.map(d => {return d.month}))
+    y.domain([0, d3.max(data, d => { return d[value] })])
 
     // X Axis
     var xAxisCall = d3.axisBottom(x)
-    g.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxisCall)
+    xAxisGroup.call(xAxisCall)
 
     // Y Axis
     var yAxisCall = d3.axisLeft(y)
         .tickFormat(d => { return "$" + d })
-    g.append("g")
-        .attr("class", "y axis")
-        .call(yAxisCall)
+    yAxisGroup.call(yAxisCall)
 
-    // Bar
+    // join new data with old elements
     var rects = g.selectAll("rect")
         .data(data)
-        
+
+    // exit old elements not present in new data
+    rects.exit().remove()
+
+    // update old elements present in new data
+    rects
+        .attr("y", d => { return y(d[value]) })
+        .attr("x", d => { return x(d.month) })
+        .attr("height", d => { return height - y(d[value]) })
+        .attr("width", x.bandwidth)
+
+    // enter new elements present in new data
     rects.enter()
         .append("rect")
-        .attr("y", d => { 
-            console.log('y ', y(d.revenue))
-            return y(d.revenue) })
+        .attr("y", d => { return y(d[value]) })
         .attr("x", d => { return x(d.month) })
-        .attr("height", d => { return height - y(d.revenue) })
+        .attr("height", d => { return height - y(d[value]) })
         .attr("width", x.bandwidth)
         .attr("fill", "grey")
-})
+
+    var label = flag ? "Revenue" : "Profit"
+    yLabel.text(label)
+}
